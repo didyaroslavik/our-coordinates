ff9ea9e1389754490d993b69f6165db397153b2a
Yaroslav <didunyaroslavik@gmail.com>
2026-07-18 22:16:47 +0300

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-50">
+      <h1 className="text-4xl font-light text-blue-900">Наші щасливі координати</h1>
+    </div>
+  )
+}
+
+export default App
